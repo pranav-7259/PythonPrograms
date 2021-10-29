6f7a3b4f36164775ec3e7ff7561e3245033d4b49
Pranav Shekhar <pranav.shekhar@learner.manipal.edu>
2021-10-29 12:34:52 -0500

    Update python_overloading.py

--- python_overloading.py
+++ python_overloading.py
@@ -8,8 +8,8 @@ def add(a, b, c):
     return a + b + c
 
 
-add(10, 20)
-add(10, 20, 30)
+print(add(10, 20))
+print(add(10, 20, 30))
 
 
 # Error as Python does not support overloading
@@ -27,5 +27,5 @@ def add(a, b, c):
     return a + b + c
 
 
-add(10, 20, 30)
-add2(40, 50)
+print(add(10, 20, 30))
+print(add2(40, 50))
