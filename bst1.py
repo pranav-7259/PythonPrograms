40805983a5d287c529a6518c24a7718818d31287
Pranav Shekhar <pranavshekhar2@gmail.com>
2022-09-17 11:34:31 -0500

    Added bst1 file

---
+++ bst1.py
@@ -0,0 +1,64 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if self.data == data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+
+        if data > self.data:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def inorder_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.inorder_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.inorder_traversal()
+
+        return elements
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+
+if __name__ == '__main__':
+    numbers = [20, 10, 15, 40, 20, 6]
+    numbers_tree = build_tree(numbers)
+    #print(numbers_tree.inorder_traversal())
+    print(numbers_tree.search(8))
+
